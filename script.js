bd6d50c909fb61fe96c72bf96a443c50fb7ee781
Evan Porter <evan.porter@dotman.ca>
2018-11-04 21:30:37 -0400

    Use API to fully get certificate as string

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 "use strict";
-
 require('dotenv').load();
 
 var client = require("dnsimple")({
-  baseUrl: 'https://api.sandbox.dnsimple.com',
-  accessToken: process.env.TOKEN,
+  accessToken: process.env.DNSIMPLE_API_TOKEN,
 });
+
+async function getDomainId(domainName) {
+  let response = await client.domains.getDomain(process.env.DNSIMPLE_ACCOUNT_ID, domainName);
+  return response.data.id;
+}
+
+async function getCertId(domainId) {
+  let response = await client.certificates.listCertificates(process.env.DNSIMPLE_ACCOUNT_ID, domainId);
+  return response.data[0].id;
+}
+
+async function getCertificate(domainId, certId) {
+  let response = await client.certificates.downloadCertificate(process.env.DNSIMPLE_ACCOUNT_ID, domainId, certId);
+  let fullChain = [response.data.server, ...response.data.chain];
+  return fullChain.join("\n");
+}
+
+async function getStuff() {
+  let domainId = await getDomainId("dotman.ca");
+  let certId = await getCertId(domainId);
+  let certificate = await getCertificate(domainId, certId);
+  console.log(certificate);
+}
+
+getStuff();
